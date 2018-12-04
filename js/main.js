cdf02cafa178aa045cd244c81739feff439ef3ec
alhana-yo <beatriz.german.rengel@gmail.com>
2018-12-04 18:04:05 +0100

    index perfectly done

--- js/main.js
+++ js/main.js
@@ -2,3 +2,28 @@
 
 
 
+
+// let face = document.querySelector('.face');
+// let head = document.querySelector('.head');
+
+// function laugh(e) {
+//     face.innerHTML =';)';
+
+//     // Add 100 buttons
+
+//     for (i=0; i=99; i++){
+
+//         document.innerHTML='<button>Hello`i`</button>';
+//         console.log(i);
+
+//     }
+
+// }
+
+// function initialPosition(e){
+//     face.innerHTML =':)';
+//     console.log(face);
+// }
+
+// face.addEventListener('click', laugh );
+// head.addEventListener('mouseout', initialPosition);
